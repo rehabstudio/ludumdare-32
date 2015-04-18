3507c617f97f7c5e81c1bdfe09ce2046d4a271ed
Neil McCallion <neil@rehabstudio.com>
2015-04-18 19:23:57 +0100

    Adding color toggle behaviour to gameStatus

--- src/gradience/status/gamestatus.js
+++ src/gradience/status/gamestatus.js
@@ -8,12 +8,28 @@ var gameStatus = {};
 // Active status of the three color wells.
 gameStatus.colorStates = {
     r: false,
-    g: true,
+    g: false,
     b: false
 };
 
 // Currnt active mixed color
 gameStatus.activeColor = '#ffffff';
 
+// Function to toggle color status.
+gameStatus.toggleColor = function(colorKey) {
+  console.log(colorKey);
+  this.colorStates[colorKey] = !this.colorStates[colorKey];
+  this.updateActiveColor();
+}
+
+// Updates the active color to match the toggled flags
+gameStatus.updateActiveColor = function() {
+  var r = (this.colorStates.r) ? 255 : 0,
+      g = (this.colorStates.g) ? 255 : 0,
+      b = (this.colorStates.b) ? 255 : 0;
+  this.activeColor = 'rgba(' + r + ',' + g + ',' + b + ',1)';
+}
+
+gameStatus.updateActiveColor();
 
 module.exports = gameStatus;
