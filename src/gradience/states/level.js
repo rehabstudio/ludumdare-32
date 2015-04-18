09395a891664adddb2095b26e17a0285c13945cb
Neil McCallion <neil@rehabstudio.com>
2015-04-18 20:01:24 +0100

    Adding color mixer controls

--- src/gradience/states/level.js
+++ src/gradience/states/level.js
@@ -1,7 +1,16 @@
+var KeyMap = require('../config/keymap'),
+    gameStatus = require('../status/gamestatus');
+
 var UI = {
     Weapon: require('../ui/weapon'),
     Score: require('../ui/score')
 };
+
+
+function _checkColorKeys() {
+
+
+}
 
 
 var LevelState = function(){};
@@ -9,9 +18,24 @@ var LevelState = function(){};
 LevelState.prototype = {
     init: function(args){
 
+        var self = this;
+
+        // Setup UI
         this.score = new UI.Score(this);
         this.weaponUI = new UI.Weapon(this);
 
+        // Bind controls to color mixer
+        this.controls = {};
+        for(var k in KeyMap.colorToggles) {
+            console.log(k);
+            this.controls['toggle' + k] = this.game.input.keyboard.addKey(KeyMap.colorToggles[k]);
+            (function(color) {
+                self.controls['toggle' + color].onDown.add(function(key) {
+                    gameStatus.toggleColor(color); 
+                }, self);
+            })(k);
+        }
+        
     },
     preload: function(){
 
