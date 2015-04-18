e2b04e4e551bc52993e3a4a5bc3cbdda5b09714e
Neil McCallion <neil@rehabstudio.com>
2015-04-18 23:40:03 +0100

    Change player handling

--- src/gradience/ecs/systems/controls.js
+++ src/gradience/ecs/systems/controls.js
@@ -35,22 +35,22 @@ var ControlsSystem = (function() {
                 return;
             }
 
-            entity.sprite.body.acceleration.set(0);
+            entity.sprite.body.velocity.set(0);
 
             if (keys.up.isDown) {
-                entity.sprite.body.acceleration.add(0, -entity.moveSpeed);
+                entity.sprite.body.velocity.add(0, -entity.moveSpeed);
             }
 
             if (keys.down.isDown) {
-                entity.sprite.body.acceleration.add(0, entity.moveSpeed);
+                entity.sprite.body.velocity.add(0, entity.moveSpeed);
             }
 
             if (keys.left.isDown) {
-                entity.sprite.body.acceleration.add(-entity.moveSpeed, 0);
+                entity.sprite.body.velocity.add(-entity.moveSpeed, 0);
             }
 
             if (keys.right.isDown) {
-                entity.sprite.body.acceleration.add(entity.moveSpeed, 0);
+                entity.sprite.body.velocity.add(entity.moveSpeed, 0);
             }
 
             if (keys.fire.isDown) {
