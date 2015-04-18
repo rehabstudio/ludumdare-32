7ba097a899dc1bf255d61d33ee82115f24240716
Neil McCallion <neil@rehabstudio.com>
2015-04-18 17:54:21 +0100

    Adding some dir structure

---
+++ src/gradience/status/gamestatus.js
@@ -0,0 +1,11 @@
+/**
+ * Game state to be stored here - lives, weapon state, score, etc.
+ * To be passed around as a require() where needed.
+ **/
+
+var gameStatus = {
+    
+
+};
+
+module.exports = gameStatus;
---
+++ src/gradience/ui/weapon.js
@@ -0,0 +1,5 @@
+/**
+ * Weapon UI goes here
+ **/
+
+ 
