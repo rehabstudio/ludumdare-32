fbcc972ab05109d56b5d9fc714ddd0b2cb5ce020
Neil McCallion <neil@rehabstudio.com>
2015-04-18 18:05:40 +0100

    Some renaming

--- src/gradience/status/gamestatus.js
+++ src/gradience/status/gamestatus.js
@@ -3,9 +3,13 @@
  * To be passed around as a require() where needed.
  **/
 
-var gameStatus = {
-    
+var gameStatus = {};
 
+// Active status of the three color wells.
+gameStatus.colorStates = {
+    r: false,
+    g: false,
+    b: false
 };
 
 module.exports = gameStatus;
