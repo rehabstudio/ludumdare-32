a993817196c39fbeac314efda97f11cad9f31c80
Ryan Grieve <ryan@rehabstudio.com>
2015-04-18 19:56:13 +0100

    adding player and basic ecs from ffs-ecs

--- src/gradience/states/level.js
+++ src/gradience/states/level.js
@@ -1,24 +1,45 @@
-var LevelState = function(){};
+'use strict';
+
+var components = require('../ecs/components');
+var factory = require('../ecs/factory');
+
+var ControlsSystem = require('../ecs/systems/controls');
+
+var LevelState = function() {};
 
 LevelState.prototype = {
-    init: function(args){
+    init: function() {
+        factory.initComponents(components);
+        ControlsSystem.init(this.game);
+    },
+    preload: function() {
+        this.load.spritesheet(
+            'player',
+            'assets/sprites/player.png',
+            110, 100
+        );
+    },
+    create: function() {
+        var player = factory.create();
+        player.addComponent(
+            'Sprite',
+            {
+                game: this.game,
+                x: 10,
+                y: 240,
+                asset: 'player'
+            }
+        );
+        player.addComponent('ControlsArrows');
+    },
+    update: function() {
+        ControlsSystem.update(factory.getAll());
+    },
+    render: function() {
 
     },
-    preload: function(){
-
-    },
-    create: function(){
-        console.log("LEVEL!");
-    },
-    update: function(){
-
-    },
-    render: function(){
-
-    },
-    shutdown: function(){
-        
-    },
+    shutdown: function() {
+    }
 };
 
 module.exports = LevelState;
