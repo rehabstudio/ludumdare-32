5fcdd55e6fdfe3d1329a359c90ddcd862ca1169d
Neil McCallion <neil@rehabstudio.com>
2015-04-19 01:40:52 +0100

    Move collisions into a System

---
+++ src/gradience/ecs/systems/collision.js
@@ -0,0 +1,40 @@
+'use strict';
+
+var Entities = {
+    Player: require('../entities/player'),
+    PlayerShot: require('../entities/playershot'),
+    Enemy: require('../entities/enemy')
+};
+
+var CollisionSystem = (function() {
+
+    var start, game;
+
+    function init(gameInstance) {
+        game = gameInstance;
+    }
+
+    function update(ents) {
+
+        game.physics.arcade.collide(Entities.Player.get(), Entities.Enemy.getGroup(), function(p, e) {
+            console.log('Player hit enemy', p, e);
+            p.kill();
+        }, null, game);
+
+        game.physics.arcade.collide(Entities.PlayerShot.getGroup(), Entities.Enemy.getGroup(), function(b, e) {
+            console.log('Bullet hit enemy', b, e);
+            b.kill();
+            e.kill();
+        }, null, game);
+
+        
+    }
+
+    return {
+        init: init,
+        update: update
+    };
+
+})();
+
+module.exports = CollisionSystem;
